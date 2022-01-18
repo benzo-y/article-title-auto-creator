f2c2df01b6a3426214797be5fc673bc9dd57d019
benzo-y <fukubenzo@gmail>
2022-01-18 12:06:54 +0900

    タイトル生成のページを追加 ヘッダに各ページのリンクを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import TitleCreater from '../views/TitleCreater.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/title_creater',
+    name: 'TitleCreater',
+    component: TitleCreater
   },
   {
     path: '/about',
