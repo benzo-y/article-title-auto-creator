63bca88fcd7988a084fdae0339fec66c1d751f96
benzo-y <fukubenzo@gmail>
2022-01-19 11:40:29 +0900

    #1 サイドナビゲーションをApp.vueに仮実装

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       color="primary"
       dark
     >
+      <v-app-bar-nav-icon @click="toggleSideMenu"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <router-link :to="{ name: 'Home'}">
         <v-btn text>
@@ -17,7 +18,26 @@
         </v-btn>            
       </router-link>
     </v-app-bar>
-
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-content>
+          タイトル生成サイト
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item :key="1" :to="{ name: 'Home'}" exact>
+          <v-list-item-content>
+            <v-list-item-title>ホーム</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item :key="2" :to="{ name: 'TitleCreater'}" exact>
+          <v-list-item-content>
+            <v-list-item-title>タイトル生成</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view/>
     </v-main>
@@ -30,7 +50,12 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false
   }),
+  methods: {
+    toggleSideMenu() {
+      this.drawer = !this.drawer;
+    }
+  }
 };
 </script>
