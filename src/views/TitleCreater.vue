329daf817ee0c590c5c504f9f6ff7298c61c59d4
benzo-y <fukubenzo@gmail>
2022-01-22 13:51:20 +0900

    #6 キーワードの値を使用して仮タイトル案リストからタイトル生成処理を実装
    入力チェックと、enterキーでsubmit抑制するように実装

--- src/views/TitleCreater.vue
+++ src/views/TitleCreater.vue
@@ -5,40 +5,33 @@
         <h1>タイトル生成</h1>
       </v-flex>
       <v-flex lg10 xs10 mb-5>
-        <v-card>
-          <v-card-title>
-            条件
-          </v-card-title>
-            <v-card-text>
-            <v-form>
-              <v-text-field label="キーワード"></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions class="justify-center">
-            <v-btn color="info">作成</v-btn>
-          </v-card-actions>
-        </v-card>
+        <v-form ref="form" @submit.prevent="createTitles">
+          <v-card>
+            <v-card-title>
+              条件
+            </v-card-title>
+              <v-card-text>
+                <v-text-field 
+                  v-model="keyword"
+                  label="キーワード"
+                  :rules="rules"
+                >
+                </v-text-field>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn color="info" @click="createTitles">作成</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
       </v-flex>
-      <v-flex lg10 xs10 mb-5>
+      <v-flex lg10 xs10 mb-5 v-if="titles.length">
         <v-card>
           <v-list>
-            <v-list-item>
+            <v-list-item v-for="(title, index) in titles" :key="index">
               <v-list-item-icon class="mr-0">
                 <v-icon x-small>mdi-checkbox-blank-circle</v-icon>
               </v-list-item-icon>
-              <v-list-item-title>【必見】○○完全解説！おすすめ10選を紹介【必見】○○完全解説！おすすめ10選を紹介</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon class="mr-0">
-                <v-icon x-small>mdi-checkbox-blank-circle</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>【必見】○○完全解説！おすすめ10選を紹介</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon class="mr-0">
-                <v-icon x-small>mdi-checkbox-blank-circle</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>【必見】○○完全解説！おすすめ10選を紹介</v-list-item-title>
+              <v-list-item-title>{{title}}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-card>
@@ -48,6 +41,41 @@
 </template>
 
 <script>
+export default{
+  data: () => ({
+    keyword: "",
+    titles: [],
+    rules: [
+      v => !!v || 'キーワードを入力してください。',
+      v => v.length <= 30 || 'キーワードは30文字以内に設定してください。',
+    ]
+  }),
+  methods: {
+    createTitles() {
+      var result = this.$refs.form.validate();
+      if (result) {
+        var title_tmplate = []
+
+        // TODO: タイトル作成用ひな形の読み込み
+        title_tmplate = [
+          ["【必見】", "完全解説！おすすめ10選を紹介"],
+          ["【おすすめ】", "完全解説！おすすめ20選を紹介"],
+          ["【一押し】", "完全解説！おすすめ30選を紹介"],
+          ["【重要】", "完全解説！おすすめ40選を紹介"],
+          ["【要チェック】", "完全解説！おすすめ50選を紹介"],
+          ["【閲覧注意】", "完全解説！おすすめ60選を紹介"],
+        ];
+
+        this.titles = [];
+        [...Array(3)].map(() => {
+          var randm_num = Math.floor( Math.random() * title_tmplate.length);
+          this.titles.push(title_tmplate[randm_num][0] + this.keyword + title_tmplate[randm_num][1]);          
+          title_tmplate.splice(randm_num, 1);
+        });
+      }
+    }
+  },
+}
 </script>
 
 <style scoped lang="scss">
