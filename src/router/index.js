20c9cf9c471480d14d26b9f5c0f78fdbb61f1d70
benzo-y <fukubenzo@gmail>
2022-01-26 11:59:43 +0900

    #12 不要なファイルと処理を削除

--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,6 @@ const routes = [
     name: 'TitleCreater',
     component: TitleCreater
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
